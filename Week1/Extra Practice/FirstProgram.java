7955199b12ce47dafa309cf102f0e3fce7e18592
ELLIE COMBS <ewxc96@gmail.com>
2022-04-05 11:10:44 -0700

    First Program

---
+++ Week1/Extra Practice/FirstProgram.java
@@ -0,0 +1,19 @@
+public class FirstProgram{
+//Entry Point
+    public static void main(String[]args){
+        name("Ellie Combs");
+        age(25);
+        place("Gig Harbor");
+    }
+private static void name(String name) {
+    System.out.println("Hello, my name is " + name + "!");
+    }
+private static void age(int age) {
+    System.out.println("I am " + age + " years old.");
+    }
+private static void place(String place) {
+    System.out.println("My hometown is " + place + ".");
+    }
+}
+
+
